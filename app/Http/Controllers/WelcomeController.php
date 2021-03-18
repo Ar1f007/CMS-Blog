a593d8d26156c83aea90f847df35b2e43d24600c
Ar1f007 <Muhammadarif9416@yahoo.com>
2021-03-18 13:12:44 +0600

    post sorted by latest first

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,12 +10,11 @@ use Illuminate\Http\Request;
 class WelcomeController extends Controller
 {
 
-    public function index(){
-
-    return view('welcome')
-    ->with('posts', Post::searched()->simplePaginate(4))
-    ->with('categories', Category::all())
-    ->with('tags', Tag::all());
-
+    public function index()
+    {
+        return view('welcome')
+        ->with('posts', Post::searched()->latest('id')->simplePaginate(4))
+        ->with('categories', Category::all())
+        ->with('tags', Tag::all());
     }
 }
